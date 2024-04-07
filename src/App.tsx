4f6d76e66674cadfe12d072f6e43efa8e0b86cb4
Gabriel Torres <gaabrieltt7@gmail.com>
2024-04-07 18:12:35 -0300

    feat: add Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import './App.css'
-import logo from './logo.svg'
+import { FC } from 'react'
 
-function App() {
+// components
+import Header from './components/header/header.component'
+
+interface AppProps {
+  message?: string
+}
+
+const App: FC<AppProps> = ({ message }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
     </div>
   )
 }
