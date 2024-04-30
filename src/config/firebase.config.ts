3afbb5b8bcc085b55ed60ff630bfd63f6bbb17a6
Gabriel Torres <gaabrieltt7@gmail.com>
2024-04-30 20:55:03 -0300

    chore: add firebase config

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDUwfPsvnfHI8DAPxK6Qj8pj9OOzBSD6Ag',
+  authDomain: 'rinho-store.firebaseapp.com',
+  projectId: 'rinho-store',
+  storageBucket: 'rinho-store.appspot.com',
+  messagingSenderId: '187726129960',
+  appId: '1:187726129960:web:aa1a9538991ffad958be1d'
+}
+
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
