c5e1b0e5dde5ba38e58a4919c7a29149a261d1cd
Gabriel Torres <gaabrieltt7@gmail.com>
2024-05-01 19:14:04 -0300

    feat: add auth state listener and sign out button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 // Pages
+import { onAuthStateChanged } from '@firebase/auth'
+import { auth } from './config/firebase.config'
 import HomePage from './pages/home/home.page'
 import LoginPage from './pages/login/login.page'
 import SignUpPage from './pages/sign-up/sign-up.page'
 
 const App = () => {
+  onAuthStateChanged(auth, (user) => {
+    console.log({ user })
+  })
   return (
     <BrowserRouter>
       <Routes>
