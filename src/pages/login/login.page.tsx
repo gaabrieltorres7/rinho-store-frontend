30db8300df379ca240fb900d0cfafbc8d6c4ae95
Gabriel Torres <gaabrieltt7@gmail.com>
2024-05-01 19:06:48 -0300

    feat: add Login and validation

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,3 +1,4 @@
+import { AuthError, AuthErrorCodes, signInWithEmailAndPassword } from '@firebase/auth'
 import { useForm } from 'react-hook-form'
 import { BsGoogle } from 'react-icons/bs'
 import { FiLogIn } from 'react-icons/fi'
@@ -6,6 +7,7 @@ import CustomButton from '../../components/custom-button/custom-button.component
 import CustomInput from '../../components/custom-input/custom-input.component'
 import Header from '../../components/header/header.component'
 import InputErrorMessage from '../../components/input-error-message/input-error-message.component'
+import { auth } from '../../config/firebase.config'
 import {
   LoginContainer,
   LoginContent,
@@ -23,10 +25,20 @@ const LoginPage = () => {
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors }
   } = useForm<LoginFormData>()
-  const handleSubmitPress = (data: LoginFormData) => {
-    console.log({ data })
+  const handleSubmitPress = async ({ email, password }: LoginFormData) => {
+    try {
+      const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+      console.log({ userCredentials })
+    } catch (error) {
+      const _error = error as AuthError
+      if (_error.code === AuthErrorCodes.INVALID_LOGIN_CREDENTIALS) {
+        setError('password', { type: 'invalidCredentials' })
+        setError('email', { type: 'invalidCredentials' })
+      }
+    }
   }
 
   return (
@@ -57,7 +69,6 @@ const LoginPage = () => {
             {errors?.email?.type === 'required' && (
               <InputErrorMessage>O email é obrigatório</InputErrorMessage>
             )}
-
             {errors?.email?.type === 'validate' && (
               <InputErrorMessage>Insira um email válido</InputErrorMessage>
             )}
@@ -71,9 +82,11 @@ const LoginPage = () => {
               placeholder='Digite sua senha'
               {...register('password', { required: true })}
             />
-
             {errors?.password?.type === 'required' && (
               <InputErrorMessage>A senha é obrigatória</InputErrorMessage>
+            )}
+            {(errors?.password?.type === 'invalidCredentials' || errors?.email?.type === 'invalidCredentials') && (
+              <InputErrorMessage>Senha ou email incorretos</InputErrorMessage>
             )}
           </LoginInputContainer>
 
