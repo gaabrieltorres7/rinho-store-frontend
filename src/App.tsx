5e10eea2f5d152e6909ed9c3c9c5cfd946781f65
Gabriel Torres <gaabrieltt7@gmail.com>
2024-05-04 17:31:35 -0300

    feat: ensure UserContext is updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,31 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 // Pages
 import { onAuthStateChanged } from '@firebase/auth'
-import { auth } from './config/firebase.config'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import { useContext } from 'react'
+import { auth, db } from './config/firebase.config'
+import { UserContext } from './contexts/user.context'
 import HomePage from './pages/home/home.page'
 import LoginPage from './pages/login/login.page'
 import SignUpPage from './pages/sign-up/sign-up.page'
 
 const App = () => {
-  onAuthStateChanged(auth, (user) => {
-    console.log({ user })
+  const { isAuthenticated, loginUser, logoutUser } = useContext(UserContext)
+
+  onAuthStateChanged(auth, async (user) => {
+    const isSigningOut = isAuthenticated && !user
+    if (isSigningOut) {
+      logoutUser()
+    }
+
+    const isSigningIn = !isAuthenticated && user
+    if (isSigningIn) {
+      const querySnapshot = await getDocs(query(collection(db, 'users'), where('id', '==', user.uid)))
+      const userFromFirestore = querySnapshot.docs[0]?.data()
+      return loginUser(userFromFirestore as any)
+    }
   })
+
   return (
     <BrowserRouter>
       <Routes>
