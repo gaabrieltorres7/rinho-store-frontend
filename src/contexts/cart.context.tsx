3e263363923504431843100750be5570cb9e590c
Gabriel Torres <gaabrieltt7@gmail.com>
2024-05-10 18:22:13 -0300

    feat: add local storage persistence to cart products

--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -1,4 +1,4 @@
-import React, { FC, createContext, useMemo, useState } from 'react'
+import React, { FC, createContext, useEffect, useMemo, useState } from 'react'
 import CartProduct from '../types/cart.types'
 import Product from '../types/products.types'
 
@@ -33,6 +33,15 @@ interface CartContextProviderProps {
 export const CartContextProvider: FC<CartContextProviderProps> = ({ children }) => {
   const [isVisible, setIsVisible] = useState(false)
   const [products, setProducts] = useState<CartProduct[]>([])
+
+  useEffect(() => {
+    const productsFromLocalStorage = JSON.parse(localStorage.getItem('cartProducts')!)
+    setProducts(productsFromLocalStorage)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('cartProducts', JSON.stringify(products))
+  }, [products])
 
   const productsTotalPrice = useMemo(() => {
     return products.reduce((acc, product) => {
