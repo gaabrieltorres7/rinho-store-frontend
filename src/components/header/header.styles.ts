9f7f73b544788c431c391adaa64efa4b151f3aab
Gabriel Torres <gaabrieltt7@gmail.com>
2024-06-11 20:41:38 -0300

    style: add responsiveness to Header Component

--- src/components/header/header.styles.ts
+++ src/components/header/header.styles.ts
@@ -8,33 +8,55 @@ export const HeaderContainer = styled.div`
   justify-content: space-between;
   padding: 20px;
   color: ${Colors.text.white};
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 `
 
-// exsc para utilizar snippets
 export const HeaderTitle = styled.h2`
   font-weight: bold;
   font-size: 1.5rem;
-  &:hover{
+
+  &:hover {
     cursor: pointer;
+  }
+
+  @media (max-width: 768px) {
+    margin-bottom: 16px;
   }
 `
 
 export const HeaderItems = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    justify-content: space-evenly;
+  }
 `
 
 export const HeaderItem = styled.div`
-  font-weight: bold;
+  font-weight: 600;
   font-size: 1rem;
   display: flex;
   align-items: center;
-  &:nth-child(1), 
-  &:nth-child(2), 
-  &:nth-child(3){
+
+  &:nth-child(1),
+  &:nth-child(2),
+  &:nth-child(3) {
     margin-right: 40px;
+
+    @media (max-width: 768px) {
+      margin-right: 0;
+    }
   }
-  &:hover{
+
+  &:hover {
     cursor: pointer;
   }
 `
